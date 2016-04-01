36d4c0f24dda558ef33b7479f3275adde1c899c9
Thomas Spalinger <spali@spali.ch>
2016-04-01 10:55:55 -0400

    switch to alpine base

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,29 @@
-FROM debian:wheezy
+FROM alpine:3.3
 
 #######################################################################################
 
+# Version of maven to be installed
+ENV MAVEN_VERSION 3.2.5
+# Version of tomcat to be installed
+ENV TOMCAT_VERSION 7.0.68
 # Version of guacamole to be installed
-ENV GUAC_VER 0.9.6
+ENV GUAC_VER 0.9.9
 # Version of mysql-connector-java to install
 ENV MCJ_VER 5.1.32
 # Version of postgresql-connector-java to install
 ENV PCJ_VER 9.4-1201
 
 # prepare apt and system (first clean is required to prevent gpg keys errors)
-RUN apt-get clean && \
-	apt-get update && \
-	DEBIAN_FRONTEND=noninteractive apt-get upgrade -y && \
-	apt-get clean
+RUN apk --update add curl
 
 ## install required apackages
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends supervisor tomcat7 && \
-	apt-get clean
+RUN curl "https://archive.apache.org/dist/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz" | tar -xzC /tmp
+RUN mv /tmp/apache-tomcat* /usr/tomcat
+RUN apk --update add supervisor openjdk7-jre
 
 # build and install guacamole and related components (wrapped script to minimize image commits and keep images small)
 COPY build.sh /
-RUN /bin/bash /build.sh
+RUN /bin/sh /build.sh
 RUN rm /build.sh
 
 # add sample configuration for no auth
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 #######################################################################################
 # prepare apt
@@ -8,71 +8,70 @@ export DEBIAN_FRONTEND=noninteractive
 echo "---------------------------------------------------------------------------------------"
 echo " install required dependencies to build guacamole"
 echo "---------------------------------------------------------------------------------------"
-apt-get update
-apt-get install -y --no-install-recommends git-core wget \
-	default-jdk maven2 \
-	libtool autoconf automake make \
-	libcairo2-dev libpng12-dev libossp-uuid-dev \
-	libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev \
+apk --update add libtool autoconf automake git build-base \
+	libpng-dev libjpeg-turbo-dev
+curl "http://ftp.unicamp.br/pub/apache/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz" | tar -xzC /tmp
+mv /tmp/apache-maven-* /tmp/maven
+#apt-get update
+#apt-get install -y --no-install-recommends git-core wget \
+	#-jdk maven2 \
+	#libtool autoconf automake make \
+	#libcairo2-dev libpng12-dev libossp-uuid-dev \
+	#libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev \
 
 
 echo "---------------------------------------------------------------------------------------"
 echo " install and build guacamole server"
 echo "---------------------------------------------------------------------------------------"
-(
-	git clone https://github.com/glyptodon/guacamole-server.git -b $GUAC_VER --single-branch /tmp/guacamole-server
-	cd /tmp/guacamole-server
-	autoreconf -fi
-	./configure --with-init-dir=/etc/init.d
-	make
-	make install
-	update-rc.d guacd defaults
-	ldconfig
-	ln -s /etc/guacamole /usr/share/tomcat7/.guacamole
-)
+git clone https://github.com/glyptodon/guacamole-server.git -b $GUAC_VER --single-branch /tmp/guacamole-server
+cd /tmp/guacamole-server
+autoreconf -fi
+./configure --with-init-dir=/etc/init.d || cat config.log && return false
+make
+make install
+update-rc.d guacd defaults
+ldconfig
+ln -s /etc/guacamole /usr/share/tomcat7/.guacamole
 
 
 echo "---------------------------------------------------------------------------------------"
 echo " build and install guacamole client"
 echo "---------------------------------------------------------------------------------------"
-(
-	git clone https://github.com/glyptodon/guacamole-client -b $GUAC_VER --single-branch /tmp/guacamole-client
-	cd /tmp/guacamole-client
-	mvn package
-	rm -Rf /var/lib/tomcat7/webapps/*
-	cp ./guacamole/target/guacamole-$GUAC_VER.war /var/lib/tomcat7/webapps/
-	ln -s /var/lib/tomcat7/webapps/guacamole-$GUAC_VER.war /var/lib/tomcat7/webapps/ROOT.war
-	ln -s /var/lib/tomcat7/webapps/guacamole-$GUAC_VER.war /var/lib/tomcat7/webapps/guacamole.war
-	# install extensions
-	mkdir -p /etc/guacamole /var/lib/guacamole/classpath /tmp/guacamole-modules
-	cp ./extensions/guacamole-auth-ldap/target/guacamole-auth-ldap-$GUAC_VER.jar /var/lib/guacamole/classpath/
-	cp ./extensions/guacamole-auth-noauth/target/guacamole-auth-noauth-$GUAC_VER.jar /var/lib/guacamole/classpath/
-	cp ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/target/guacamole-auth-jdbc-base-$GUAC_VER.jar /var/lib/guacamole/classpath/
-	cp ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-mysql/target/extension/guacamole-auth-jdbc-mysql-$GUAC_VER.jar /var/lib/guacamole/classpath/
-	cp ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-postgresql/target/extension/guacamole-auth-jdbc-postgresql-$GUAC_VER.jar /var/lib/guacamole/classpath/
-	# Download dependancies for postgresql authentication module
-	wget -q --span-hosts -O /var/lib/guacamole/classpath/postgresql-${PCJ_VER}.jdbc41.jar https://jdbc.postgresql.org/download/postgresql-${PCJ_VER}.jdbc41.jar
-	# Download dependancies for mysql authentication module
-	wget -q --span-hosts -O /tmp/guacamole-modules/mysql-connector-java-${MCJ_VER}.tar.gz http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${MCJ_VER}.tar.gz
-	tar -zxf /tmp/guacamole-modules/mysql-connector-java-${MCJ_VER}.tar.gz -C /tmp/guacamole-modules/
-	cp /tmp/guacamole-modules/mysql-connector-java-${MCJ_VER}/mysql-connector-java-${MCJ_VER}-bin.jar /var/lib/guacamole/classpath/
-	# Copy sql shema files
-	mkdir -p /root/mysql
-	find ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-mysql -name '*.sql' -exec cp {} /root/mysql/ \;
-	mkdir -p /root/postgresql
-	find ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-postgresql -name '*.sql' -exec cp {} /root/postgresql/ \;
-)
+git clone https://github.com/glyptodon/guacamole-client -b $GUAC_VER --single-branch /tmp/guacamole-client
+cd /tmp/guacamole-client
+/tmp/maven/bin/mvn package
+rm -Rf /var/lib/tomcat7/webapps/*
+cp ./guacamole/target/guacamole-$GUAC_VER.war /var/lib/tomcat7/webapps/
+ln -s /var/lib/tomcat7/webapps/guacamole-$GUAC_VER.war /var/lib/tomcat7/webapps/ROOT.war
+ln -s /var/lib/tomcat7/webapps/guacamole-$GUAC_VER.war /var/lib/tomcat7/webapps/guacamole.war
+# install extensions
+mkdir -p /etc/guacamole /var/lib/guacamole/classpath /tmp/guacamole-modules
+cp ./extensions/guacamole-auth-ldap/target/guacamole-auth-ldap-$GUAC_VER.jar /var/lib/guacamole/classpath/
+cp ./extensions/guacamole-auth-noauth/target/guacamole-auth-noauth-$GUAC_VER.jar /var/lib/guacamole/classpath/
+cp ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-base/target/guacamole-auth-jdbc-base-$GUAC_VER.jar /var/lib/guacamole/classpath/
+cp ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-mysql/target/extension/guacamole-auth-jdbc-mysql-$GUAC_VER.jar /var/lib/guacamole/classpath/
+cp ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-postgresql/target/extension/guacamole-auth-jdbc-postgresql-$GUAC_VER.jar /var/lib/guacamole/classpath/
+# Download dependancies for postgresql authentication module
+curl -o /var/lib/guacamole/classpath/postgresql-${PCJ_VER}.jdbc41.jar https://jdbc.postgresql.org/download/postgresql-${PCJ_VER}.jdbc41.jar
+# Download dependancies for mysql authentication module
+curl -o /tmp/guacamole-modules/mysql-connector-java-${MCJ_VER}.tar.gz http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${MCJ_VER}.tar.gz
+tar -zxf /tmp/guacamole-modules/mysql-connector-java-${MCJ_VER}.tar.gz -C /tmp/guacamole-modules/
+cp /tmp/guacamole-modules/mysql-connector-java-${MCJ_VER}/mysql-connector-java-${MCJ_VER}-bin.jar /var/lib/guacamole/classpath/
+# Copy sql shema files
+mkdir -p /root/mysql
+find ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-mysql -name '*.sql' -exec cp {} /root/mysql/ \;
+mkdir -p /root/postgresql
+find ./extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-postgresql -name '*.sql' -exec cp {} /root/postgresql/ \;
+
 
 echo "---------------------------------------------------------------------------------------"
 echo " install guacamole modules"
 echo "---------------------------------------------------------------------------------------"
 # Build and install HMAC Authentication module
-(
-	git clone https://github.com/grncdr/guacamole-auth-hmac.git /tmp/guacamole-modules/guacamole-auth-hmac
-	cd /tmp/guacamole-modules/guacamole-auth-hmac
-	mvn package
-	cp ./target/guacamole-auth-hmac-*-SNAPSHOT.jar /var/lib/guacamole/classpath/
-)
+git clone https://github.com/grncdr/guacamole-auth-hmac.git /tmp/guacamole-modules/guacamole-auth-hmac
+cd /tmp/guacamole-modules/guacamole-auth-hmac
+/tmp/maven/bin/mvn package
+cp ./target/guacamole-auth-hmac-*-SNAPSHOT.jar /var/lib/guacamole/classpath/
 
 
 echo "---------------------------------------------------------------------------------------"
